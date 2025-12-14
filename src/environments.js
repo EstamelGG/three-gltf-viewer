cde64d3116f37a63f219c373e5d5fd17d10fba02
GG <46676047+EstamelGG@users.noreply.github.com>
2025-12-14 20:31:10 +0800

    1

--- src/environments.js
+++ src/environments.js
@@ -11,9 +11,33 @@ export const environments = [
 		format: '.cube',
 	},
 	{
-		id: 'a13_cube',
+		id: 'amarr',
 		name: 'Amarr',
-		path: './src/envImage/a13_cube',
+		path: './src/envImage/Amarr',
+		format: '.cube',
+	},
+	{
+		id: 'abyssal',
+		name: 'Abyssal',
+		path: './src/envImage/Abyssal',
+		format: '.cube',
+	},
+	{
+		id: 'gallent',
+		name: 'Gallent',
+		path: './src/envImage/Gallent',
+		format: '.cube',
+	},
+	{
+		id: 'caldari',
+		name: 'Caldari',
+		path: './src/envImage/Caldari',
+		format: '.cube',
+	},
+	{
+		id: 'minmatar',
+		name: 'Minmatar',
+		path: './src/envImage/Minmatar',
 		format: '.cube',
 	}
 ];
